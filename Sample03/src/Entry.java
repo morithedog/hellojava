0abb28b98698c71851baa22bbc9811a96952206b
rucio <rucio@rucio-PC>
2015-03-22 13:08:26 +0900

    test

---
+++ Sample03/src/Entry.java
@@ -0,0 +1,17 @@
+
+public class Entry {
+
+	public static void main(String[] args){
+		
+		Test1 t1 = new Test1();
+		t1.Value1 = "Apple";
+		t1.Value2 = "Banana";
+		
+		String result;
+		
+		result = t1.Say();
+		
+		System.out.println(result);
+		
+	}
+}
---
+++ Sample03/src/Test1.java
@@ -0,0 +1,10 @@
+
+public class Test1 {
+
+	public String Value1;
+	public String Value2;
+	
+	public String Say(){
+		return this.Value1 + this.Value2;
+	}
+}
